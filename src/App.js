0b0a3efb02a076b8f3e2bd1ce6c2634152e2dfd9
gowthamsmik <gowtham.smiksystems@gmail.com>
2023-05-21 21:23:42 +0530

    menubar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='row border'>
+        <div className='col'><App1 /></div>
+      
+      </div>
+    </>
   );
-}
+};
+
+function App1() {
+  return (
+    <>
+      <div className='threedbox'>
+        <ul>
+          <li className="--i:5;"><h5>Home</h5></li>
+          <li className="--i:4;"><h5>About</h5></li>
+          <li className="--i:3;"><h5>Services</h5></li>
+          <li className="--i:2;"><h5>Portfolio</h5></li>
+          <li className="--i:1;"><h5>Contact</h5></li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default App;
